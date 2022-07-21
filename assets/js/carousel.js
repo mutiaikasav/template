820ed92f2c491eec852e788dbc052bcb63ff575d
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-07-21 16:12:28 +0700

    add new menu

---
+++ assets/js/carousel.js
@@ -0,0 +1,39 @@
+/**
+* Template Name: EstateAgency - v4.7.0
+* Template URL: https://bootstrapmade.com/real-estate-agency-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+    "use strict";
+  
+    /**
+     * Property carousel
+     */
+    new Swiper('#property-carousel', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.propery-carousel-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+  
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        }
+      }
+    });
+  
+  })()
